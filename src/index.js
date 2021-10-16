a2cfcf248e8e285009ae1386ff372af3b988ccdf
LUCAS  NEGRESIOLO LIBORIO <17.01165-5@maua.br>
2021-10-16 08:39:35 -0300

    aula 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles.css'
 
 const App = () => {
+    const estiloBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, 
+        backgroundColor: 'blueviolet', color: 'white', 
+        border: 'none', width:'100%', borderRadius: 8}
+
+    const textoDoRotulo = "Nome: "
+
+    const obterTextoDoBotao = () => {
+        return "Enviar"
+    }
+    const eventoBotao = () => {
+        console.log ("Botao clicado")
+    }
     //expressão JSX
-    return <div>Meu primeiro componente ReactJS</div>
+    return (
+        <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', 
+        padding: 12, borderRadius: 8}}>
+            <label htmlFor="nome" className="rotulo" style={{display: 'block', marginBottom: 4}}>{textoDoRotulo}</label>
+            <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, 
+                borderStyle: 'hidden', width: '100%', borderRadius: 8, 
+                outline: 'none', boxSizing: 'border-box'}}/>
+            <button style={estiloBotao} onClick={eventoBotao}>
+                {obterTextoDoBotao()}
+            </button>
+        </div>
+    );
 }
 
 ReactDOM.render(
